e691fa0707a28abadfdedd6acd0af15c6d910bd9
singularity <84933379+fogInSingularity@users.noreply.github.com>
2023-08-20 09:56:58 +0300

    change main.cpp
    SNAN -> NAN
    разбил main на функции linr и quad
    разбил input на функции
    is_zero теперь нормально реализован
    sqrt() теперь считается только один раз

--- main.cpp
+++ main.cpp
@@ -3,63 +3,71 @@
 
 #define SMALL_NUMBER 0.00000001
 
-void input ( double*, double*, double* );
-int is_zero ( double );
+void read_lf(double* n);
+void input_of_coeff(double* a, double* b, double* c);
+int is_zero(double n);
+void linr(double b, double c);
+void quad(double a, double b, double c);
 
-int main () {
-	double a = SNAN;
-	double b = SNAN;
-	double c = SNAN;
+int main(){
+	double a = NAN;
+	double b = NAN;
+	double c = NAN;
 
-	input ( &a, &b, &c );
+	input_of_coeff(&a, &b, &c);
 
 	if ( is_zero(a) ) { // линейное bx + c = 0
-		printf ( "bro thats not even quadratic\n" );
-		if ( is_zero(b) ) { // b == 0
-			if ( is_zero(c) ) { // все вещ числа корни
-				printf ( "root: any real number\n" );
-			} else { // нет корней
-				printf ( "no roots\n" );
-			}
-		} else { // один корень
-			printf ( "root: %.5lf\n", (-c)/b );
-		}
+		linr(b, c);
 	} else { // квадратное ax^2 + bx + c = 0
-		double d = b*b - 4.0*a*c;
-		if ( is_zero(d) ) { // один корень 
-			printf ( "root: %.5lf\n", (-b)/(2.0*a) );
-		} else if ( d > 0.0 ) { // два корня
-			printf ( "first root: %.5lf\n", ( -b + sqrt(d) )/( 2.0 * a ) );
-			printf ( "second root: %.5lf\n", ( -b - sqrt(d) )/( 2.0 * a ) );
-		} else if ( d < 0.0 ) { // нет корней
-			printf ( "no roots\n" );
-		}
+		quad(a, b, c);
 	}
 
 	return 0;
 }
 
-void input ( double* a_p, double* b_p, double* c_p ) {
-	printf ( "enter coefficients of\nquadratic equation\n" );
-	printf ( "a: " );
+void read_lf(double* n) {
 	char ch = '\0';
-	while ( !scanf ( "%lf", a_p ) ) {
-		scanf ( "%c", &ch );
-	}
-	printf ( "b: " );
-	while ( !scanf ( "%lf", b_p) ) {
-		scanf ( "%c", &ch );
-	}
-	printf ( "c: " );
-	while ( !scanf ( "%lf", c_p ) ) {
-		scanf ( "%c", &ch );
+	while ( !scanf("%lf", n) ) {
+		scanf("%c", &ch);
 	}
 }
 
-int is_zero ( double n ) {
-	if ( abs(n) < SMALL_NUMBER ) {
-		return 1;
-	} else {
-		return 0;
+void input_of_coeff(double* a, double* b, double* c) {
+	printf("enter coefficients of\nquadratic equation\n");
+	printf("a: ");
+	read_lf(a);
+	printf("b: ");
+	read_lf(b);
+	printf("c: ");
+	read_lf(c);
+}
+
+int is_zero(double n) {
+	return abs(n) < SMALL_NUMBER;
+}
+
+void linr(double b, double c) {
+	printf("bro thats not even quadratic\n");
+	if ( is_zero(b) ) { // b == 0
+		if ( is_zero(c) ) { // все вещ числа корни
+			printf("root: any real number\n");
+		} else { // нет корней
+			printf("no roots\n");
+		}
+	} else { // один корень
+		printf("root: %.5lf\n", (-c)/b);
+	}
+}
+
+void quad(double a, double b, double c){
+	double d = b*b - 4.0*a*c;
+	double d_sqrt = sqrt(d);
+	if ( is_zero(d) ) { // один корень 
+		printf("root: %.5lf\n", (-b)/(2.0*a));
+	} else if ( d > 0.0 ) { // два корня
+		printf("first root: %.5lf\n", ( -b + d_sqrt ) / ( 2.0 * a ) );
+		printf("second root: %.5lf\n", ( -b - d_sqrt ) / ( 2.0 * a ) );
+	} else if ( d < 0.0 ) { // нет корней
+		printf ("no roots\n");
 	}
 }
